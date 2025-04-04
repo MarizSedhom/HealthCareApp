cd5bd4a4a3637d3536c99cc5fae9ac65c56f6628
Tarek Khaled <tarekkhaled5112000@yahoo.com>
2025-04-04 15:19:37 +0200

    .

---
+++ HealthCareApp/ViewModels/SubSpecializationVM.cs
@@ -0,0 +1,19 @@
+﻿using HealthCareApp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthCareApp.ViewModels
+{
+    public class SubSpecializationVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z&,\s]+(?:\s\([a-zA-Z&,\s]+\))?$", ErrorMessage = "Invalid format. The string must contain only letters, ampersands, commas, spaces, and optionally, a description in parentheses.")]
+        [Unique<SubSpecialization>("Name", ErrorMessage = "The name is already in use.")]
+        public string Name { get; set; }
+
+        public int SpecializationId { get; set; }
+        public IEnumerable<Specialization>? Specializations { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
