3b182410aaba4a80c61465e896b52b4d80e19365
Heba-abdelwahab <heba.abdelwahab.sayed@gmail.com>
2025-04-21 21:12:25 +0200

    deleteIgenericRepoDoctor

--- HealthCareApp/RepositoryServices/IDoctorRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HealthCareApp.RepositoryServices
-{
-    public interface IDoctorRepository:IGenericRepoServices<Doctor>
-    {
-        Doctor GetDrWithClinicAvailabilities(string doctorId);
-
-    }
-}
