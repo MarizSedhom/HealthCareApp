60c0e77d033ca46ddbb48eebe24d9c1ada6a1b77
MarizSedhom <marizsedhom@gmail.com>
2025-04-21 09:24:17 +0200

    Adding Patient Profile and edit profile Make the Default routing for patient the GetAllDoctors

--- HealthCareApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HealthCareApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -22,11 +22,13 @@ namespace HealthCareApp.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -112,10 +114,18 @@ namespace HealthCareApp.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var user = await _userManager.FindByEmailAsync(Input.Email);
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
+                    var roles = await _userManager.GetRolesAsync(user);
+
+                    // Redirect based on role
+                    if (roles.Contains("Patient"))
+                    {
+                        return RedirectToAction("GetAllDoctorsInfo", "Doctor");
+                    }
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
